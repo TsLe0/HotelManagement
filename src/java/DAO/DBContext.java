ee55390578eec64272e8edd557b5e4091e72373d
mzxv1 <tuquyenhbt@gmail.com>
2025-06-09 00:21:49 +0700

    login update

--- src/java/DAO/DBContext.java
+++ src/java/DAO/DBContext.java
@@ -6,20 +6,34 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-
 public class DBContext {
+
     private final String serverName = "localhost";
     private final String dbName = "HotelManagement";
     private final String portNumber = "1433";
     private final String userID = "sa";
     private final String password = "123";
+    protected Connection connection;
+
     
+    //this is a horrible way but time is short, if we can group these 2 together then it would be nice --MinhN
+    public DBContext() {
+        try {
+            // Edit URL , username, password to authenticate with your MS SQL Server
+            String url = "jdbc:sqlserver://" + serverName + ":" + portNumber + ";databaseName=" + dbName + ";encrypt=true;trustServerCertificate=true;";
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connection = DriverManager.getConnection(url, userID, password);
+        } catch (ClassNotFoundException | SQLException ex) {
+            System.out.println(ex);
+        }
+    }
+
     public Connection getConnection() throws Exception {
-        String url = "jdbc:sqlserver://" + serverName + ":" + portNumber + ";databaseName=" + dbName+";encrypt=true;trustServerCertificate=true;";
+        String url = "jdbc:sqlserver://" + serverName + ":" + portNumber + ";databaseName=" + dbName + ";encrypt=true;trustServerCertificate=true;";
         Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
         return DriverManager.getConnection(url, userID, password);
     }
-    
+
     public void closeConnection(Connection con, PreparedStatement ps, ResultSet rs) throws SQLException {
         if (rs != null && !rs.isClosed()) {
             rs.close();
@@ -30,5 +44,5 @@ public class DBContext {
         if (con != null && !con.isClosed()) {
             con.close();
         }
-    }    
+    }
 }
